e8be686616f0120173340701c30fb7dab775de20
royalturd <samridhh.rudra@hotmail.com>
2020-09-07 15:32:19 +0530

    update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""join this group forr help-- @torrentleechgdrivesupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text("""welcome to Royal_Turd's leech group""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
@@ -46,7 +46,7 @@ async def rename_message_f(client, message):
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
             text="read this?",
-            url="https://t.me/keralagram/698909"
+            url="https://t.me/rtleech/298"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
